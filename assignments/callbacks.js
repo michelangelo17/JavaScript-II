ecc9ccab7bc18af5f09c586f6d07c5c7b1258cd9
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-14 21:28:47 -0700

    completed callbacks.js challenges

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,28 +38,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, "apples"]
+const newVar = []
+function testcb(v) {
+  newVar.push(v);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let sum = x + y;
+  return cb(sum);
 }
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let product = x * y;
+  return cb(product);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let checkItem = list.filter(el => el === item)
+  if (checkItem !== []) {
+    cb(checkItem);
+  }
 }
 
+
+
+contains(1, testArr, testcb);
+console.log(newVar);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
